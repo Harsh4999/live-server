52b804e33366ae926b648c319127ce2875301e8b
Harsh <trivediharsh49@gmail.com>
2023-04-14 19:37:34 +0530

    live

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,4 @@
 local live_server = require('../lua/live-server')
 print("now will map live_server")
 live_server.start_server()
-vim.api.nvim_set_keymap('n','<leader>aaa','<cmd>lua live_server.start_server()<cr>',{noremap=true,silent=true})
 
