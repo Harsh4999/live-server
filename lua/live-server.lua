af70a6925de4b7c8ef43389f888ffcb9b940dd4e
Harsh <trivediharsh49@gmail.com>
2023-04-14 19:36:27 +0530

    commnad

--- lua/live-server.lua
+++ lua/live-server.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.start_server = function ()
-  vim.api.nvim_cmd({'!live-server --port 3000'},{})
+  vim.api.nvim_command('!live-server --port 3000')
   print("Live server started at port 3000")
 end
 
